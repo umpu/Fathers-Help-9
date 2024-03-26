cf8476d35d35c7c20087d186b65233589bbf1b0a
umpu <carol.ledgers0h@icloud.com>
2024-03-26 20:22:50 +0500

    Mask, canvas, symbols, filters

--- Fathers-Help-9/ContentView.swift
+++ Fathers-Help-9/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var offset: CGSize = .zero
+    @State private var point = CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            Rectangle()
+                .fill(
+                    RadialGradient(
+                        gradient: .init(colors: [Color.yellow, Color.red]),
+                        center: .center,
+                        startRadius: 55,
+                        endRadius: 110
+                    )
+                )
+                .mask {
+                    Canvas { context, size in
+                        let symbolOne = context.resolveSymbol(id: 0)!
+                        let symbolTwo = context.resolveSymbol(id: 1)!
+                        
+                        context.addFilter(.alphaThreshold(min: 0.5, color: .yellow))
+                        context.addFilter(.blur(radius: 30))
+                        
+                        context.drawLayer { context in
+                            context.draw(symbolOne, at: point)
+                            context.draw(symbolTwo, at: point)
+                        }
+                    } symbols: {
+                        Circle().frame(width: 110).tag(0)
+                        Circle().frame(width: 111).tag(1)
+                            .offset(x: offset.width, y: offset.height)
+                        
+                    }
+                }
+                .overlay {
+                    Image(systemName: "cloud.sun.rain.fill")
+                        .font(.system(size: 40))
+                        .symbolRenderingMode(.hierarchical)
+                        .foregroundStyle(.white)
+                        .offset(x: offset.width, y: offset.height)
+                }
+                .gesture(DragGesture()
+                    .onChanged { value in
+                        offset = value.translation
+                    }
+                    .onEnded { _ in
+                        withAnimation(.bouncy) {
+                            offset = .zero
+                        }
+                    }
+                )
+                .ignoresSafeArea()
+                .background(.black)
         }
-        .padding()
     }
 }
 

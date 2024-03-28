02ccee32a2a41d8d6a4c4c3e5f521e8879460fdd
umpu <carol.ledgers0h@icloud.com>
2024-03-28 19:51:04 +0500

    Perfect

--- Fathers-Help-9/ContentView.swift
+++ Fathers-Help-9/ContentView.swift
@@ -8,58 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset: CGSize = .zero
-    @State private var point = CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
+    @State var offset: CGSize = .zero
+    
+    var ball: some View {
+        Circle()
+            .frame(width: 150, height: 150)
+    }
     
     var body: some View {
-        GeometryReader { geometry in
-            Rectangle()
-                .fill(
-                    RadialGradient(
-                        gradient: .init(colors: [.yellow, .red]),
-                        center: .center,
-                        startRadius: 55,
-                        endRadius: 110
-                    )
-                )
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            RadialGradient(colors: [.yellow, .red], center: .center, startRadius: 70, endRadius: 160)
                 .mask {
                     Canvas { context, size in
-                        let symbol_0 = context.resolveSymbol(id: 0)!
-                        let symbol_1 = context.resolveSymbol(id: 1)!
+                        context.addFilter(.alphaThreshold(min: 0.5))
+                        context.addFilter(.blur(radius: 40))
                         
-                        context.addFilter(.alphaThreshold(min: 0.5, color: .yellow))
-                        context.addFilter(.blur(radius: 30))
-                        
-                        context.drawLayer { context in
-                            context.draw(symbol_0, at: point)
-                            context.draw(symbol_1, at: point)
+                        context.drawLayer { ctx in
+                            for index in [1, 2] {
+                                if let resolved = context.resolveSymbol(id: index) {
+                                    ctx.draw(resolved, at: .init(x: size.width / 2, y: size.height / 2))
+                                }
+                            }
                         }
                     } symbols: {
-                        Circle().frame(width: 110).tag(0)
-                        Circle().frame(width: 111).tag(1)
-                            .offset(x: offset.width, y: offset.height)
+                        ball.tag(1)
+                        ball.tag(2)
+                            .offset(offset)
                     }
                 }
-                .overlay {
-                    Image(systemName: "cloud.sun.rain.fill")
-                        .font(.system(size: 40))
-                        .symbolRenderingMode(.hierarchical)
-                        .foregroundStyle(.white)
-                        .offset(x: offset.width, y: offset.height)
-                }
-                .gesture(DragGesture()
-                    .onChanged { value in
-                        offset = value.translation
-                    }
-                    .onEnded { _ in
-                        withAnimation(.bouncy) {
-                            offset = .zero
-                        }
-                    }
-                )
-                .ignoresSafeArea()
-                .background(.black)
+            
+            Image(systemName: "cloud.sun.rain.fill")
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80)
+                .offset(offset)
+                .foregroundStyle(.white)
         }
+        .gesture(DragGesture()
+            .onChanged { drag in
+                offset = drag.translation
+            }
+            .onEnded { _ in
+                withAnimation{
+                    offset = .zero
+                }
+            }
+        )
+        
     }
 }
 
